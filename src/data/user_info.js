e80f37ffb81e52939d0e0f0061cc2f491312e3c2
Avinash Jat <dev.avinashjat@gmail.com>
2026-03-31 11:21:42 +0530

    update infomation like certificate and exprience

--- src/data/user_info.js
+++ src/data/user_info.js
@@ -1,156 +1,162 @@
 const info = {
-    // ============ MAIN DETAILS ============
-    main: {
-        name: "Avinash Jat",
-        description: "I am a software engineer with expertise in full-stack web development. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards.",
-        role: "Full-Stack Developer",
-        photo: "../Avinash_jat_1.png",
-        email: "dev.avinashjat@gmail.com",
+  // ============ MAIN DETAILS ============
+  main: {
+    name: "Avinash Jat",
+    description:
+      "I am a software engineer with expertise in full-stack web development. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards.",
+    role: "Full-Stack Developer",
+    photo: "../Avinash_jat_1.png",
+    email: "dev.avinashjat@gmail.com",
+  },
+
+  // ============ SOCIAL LINKS ============
+  socials: {
+    twitter: "https://x.com/Avi_jaat_",
+    github: "https://github.com/Avinashjat",
+    linkedin: "https://www.linkedin.com/in/avinash-jat-227776237",
+    instagram: "https://instagram.com/mr._avi_jaat",
+    leetcode: "https://leetcode.com/Avi_jaat",
+  },
+
+  // ============ PROJECTS ============
+  projects: [
+    {
+      title: "DocLink",
+      description:
+        "DocLink is a MERN stack-based doctor appointment booking system. It allows patients to schedule appointments, manage bookings, and connect with doctors seamlessly.",
+      technologies: "React, Tailwind, Express, MongoDb , Node",
+      github: "https://github.com/Avinashjat/DocLink",
+      link: "https://doclink-doctor-2gk4.onrender.com",
     },
 
-    // ============ SOCIAL LINKS ============
-    socials: {
-        twitter: "https://x.com/Avi_jaat_",
-		github: "https://github.com/Avinashjat",
-		linkedin: "https://www.linkedin.com/in/avinash-jat-227776237",
-		instagram: "https://instagram.com/mr._avi_jaat",
-		leetcode: "https://leetcode.com/Avi_jaat",
+    {
+      title: "SnapBite",
+      description:
+        "Developed a MERN stack food ordering website with secure authentication, responsive design, and real-time order tracking for a seamless user experience.",
+      technologies: "React, Tailwind , Express, MongoDb , Node",
+      github: "https://github.com/Avinashjat/SnapBite",
+      link: "https://snapbite-food.onrender.com/",
     },
 
-    // ============ PROJECTS ============
-    projects: [
-        {
-			title: "DocLink",
-			description: "DocLink is a MERN stack-based doctor appointment booking system. It allows patients to schedule appointments, manage bookings, and connect with doctors seamlessly.",
-            technologies: "React, Tailwind, Express, MongoDb , Node",
-			github: "https://github.com/Avinashjat/DocLink",
-            link: "https://doclink-doctor-2gk4.onrender.com"
-		},
+    {
+      title: "Lumix AI",
+      description:
+        "Built Lumix AI, a multi-feature AI web platform providing content generation, image processing, and resume analysis using multiple free APIs.Integrated Clerk authentication to ensure secure and seamless user access.",
+      technologies: "React, Tailwind, Express, Postgres , Node , clerk ",
+      github: "https://github.com/Avinashjat/Lumix.ai-Web-App",
+      link: "https://github.com/Avinashjat/Lumix.ai-Web-App",
+    },
 
-        {
-			title: "SnapBite",
-			description: "Developed a MERN stack food ordering website with secure authentication, responsive design, and real-time order tracking for a seamless user experience.",
-			technologies: "React, Tailwind , Express, MongoDb , Node",
-			github: "https://github.com/Avinashjat/SnapBite",
-            link: "https://snapbite-food.onrender.com/"
-		},
+    {
+      title: "Coffeeca",
+      description:
+        "Designed a modern coffee shop webpage with a responsive layout, multiple option  , interactive menu, and seamless user experience for online browsing.    ",
+      technologies: "HTML5, CSS3, Bootstrap , Javascript",
+      github: "https://github.com/Avinashjat/Coffeeca",
+      link: "",
+    },
 
-        {
-			title: "Lumix AI",
-			description: "Built Lumix AI, a multi-feature AI web platform providing content generation, image processing, and resume analysis using multiple free APIs.Integrated Clerk authentication to ensure secure and seamless user access.",
-			technologies: "React, Tailwind, Express, Postgres , Node , clerk ",
-			github: "https://github.com/Avinashjat/Lumix.ai-Web-App",
-            link: "https://github.com/Avinashjat/Lumix.ai-Web-App"
-		},
+    {
+      title: "AVI Portfolio",
+      description:
+        "Developed a responsive React portfolio website with dynamic components, smooth navigation, modern UI/UX, and integrated contact functionality.",
+      technologies: "React, Tailwind",
+      github: "https://github.com/Avinashjat/avinash-portfolio",
+      link: "https://avinash-portfolio-one.vercel.app/",
+    },
+  ],
 
-        {
-			title: "Coffeeca",
-			description: "Designed a modern coffee shop webpage with a responsive layout, multiple option  , interactive menu, and seamless user experience for online browsing.    ",
-			technologies: "HTML5, CSS3, Bootstrap , Javascript",
-			github: "https://github.com/Avinashjat/Coffeeca",
-            link: ""
-		},
+  // ============ EDUCATION ============
+  education: [
+    {
+      school: "Poornima College of Engineering",
+      degree: "Bachelor of Computer Science",
+      duration: "2021 - 2025",
+      image: "pce.png",
+    },
+    {
+      school: "Govt. Senior Secoundary School ",
+      degree: "Physics, Chemistry, and Mathematics",
+      duration: "2019 - 2021",
+      image: "rbse.jpg",
+    },
+  ],
 
-		{
-			title: "AVI Portfolio",
-			description: "Developed a responsive React portfolio website with dynamic components, smooth navigation, modern UI/UX, and integrated contact functionality.",
-			technologies: "React, Tailwind",
-			github: "https://github.com/Avinashjat/avinash-portfolio",
-            link: "https://avinash-portfolio-one.vercel.app/"
-		},
+  // ============ EXPERIENCE ============
+  experience: [
+    {
+      position: "Software Development Engineer (SDE) Intern",
+      company: "VAPPR TECH",
+      duration: "Feb 2025 - July 2025 (6 Months)",
+      image: "vapprtech-logo.png",
+      descriptions: [
+        "Developed full-stack applications using React, Node.js, Python, and SQL.",
+        "Implemented JWT/OAuth authentication and built REST APIs with database integration.",
+        "Deployed applications and collaborated on debugging and performance improvements.",
+      ],
+    },
 
-    ],
+    {
+      position: "MERN Stack Developer Trainee",
+      company: "Learn And Build",
+      duration: "july 2024 - Oct 2024 (3 Months)",
+      image: "learn_and_build_logo.jpeg",
+      descriptions: [
+        "Completed an extensive training program in MERN stack development.",
+        "Developed and deployed RESTful APIs for seamless frontend-backend communication",
+        "Mentored by senior developers on best practices and code optimization.",
+      ],
+    },
+    {
+      position: "Work Immersion - Web Developer",
+      company: "Techienest",
+      duration: "july 2022 - Sep 2022 (2 Months)",
+      image: "techienest_logo.jpeg",
+      descriptions: [
+        "Designed responsive and user-friendly interfaces with javascript and Tailwind CSS",
+        "Continuously learning and applying modern web development trends and best practices.",
+      ],
+    },
+  ],
 
-    // ============ EDUCATION ============
-    education: [
-        {
-            school: "Poornima College of Engineering",
-            degree: "Bachelor of Computer Science",
-            duration: "2021 - 2025",
-            image: "pce.png"
-        },
-        {
-            school: "Govt. Senior Secoundary School ",
-            degree: "Physics, Chemistry, and Mathematics",
-            duration: "2019 - 2021",
-            image: "rbse.jpg"
-        },
-    ],
+  // ============ CERTIFICATES ============
+  certificates: [
+    {
+      title: "Software Development Engineer (SDE) Intern",
+      description: "Provided by Vappr Tech",
+      icon: "google",
+      link: "https://drive.google.com/file/d/1fK4pw5bzPAhJ9jwSzzAVml9ovCRInTsX/view?usp=sharing",
+    },
+    {
+      title: "MERN Stack Development Certificate",
+      description: "Provided by Learn and Build",
+      icon: "ibm",
+      link: "https://drive.google.com/file/d/1CfSARMiv1Wqa5ETFqS19zr36WNXmaA-v/view?usp=sharing",
+    },
+    {
+      title: "Web Development",
+      description: "Provided by Techienest",
+      icon: "techienest_logo.jpeg",
+      link: "https://drive.google.com/file/d/1AiJ09mlHyxIcJj6nPw-Gnv9oMA8rto2i/view?usp=drive_link",
+    },
 
-    // ============ EXPERIENCE ============
-    experience: [
-        {
-            position: "Full-Stack Developer Trainee",
-            company: "InternPe",
-            duration: "July 2024 - Sep 2024 (3 Months)",
-            image: "InternPe_logo.jpeg",
-            descriptions: [
-                "Implemented secure authentication and authorization using JWT and OAuth.",
-                "Gained hands-on experience in deploying applications on cloud platforms like Vercel and Netlify.",
-                "Collaborated with a team to enhance and maintain a full-stack web application.",
-            ]
-        },
-
-        {
-            position: "MERN Stack Developer Trainee",
-            company: "Learn And Build",
-            duration: "july 2023 - Oct 2023 (3 Months)",
-            image: "learn_and_build_logo.jpeg",
-            descriptions: [
-                "Completed an extensive training program in MERN stack development.",
-                "Developed and deployed RESTful APIs for seamless frontend-backend communication",
-                "Mentored by senior developers on best practices and code optimization.",
-            ]
-        },
-        {
-            position: "Work Immersion - Web Developer",
-            company: "Techienest",
-            duration: "july 2022 - Sep 2022 (2 Months)",
-            image: "techienest_logo.jpeg",
-            descriptions: [
-                "Designed responsive and user-friendly interfaces with javascript and Tailwind CSS",
-                "Continuously learning and applying modern web development trends and best practices."
-            ]
-        },
-    ],
-
-    // ============ CERTIFICATES ============
-    certificates: [
-        {
-            title: "MERN Stack Development Certificate",
-            description: "Provided by Learn and Build",
-            icon: "ibm",
-            link: "https://drive.google.com/file/d/1QbqOpnJjMblIAxBGG93r8706LzSLUwoT/view?usp=drive_link",
-        },
-        {
-            title: "Full Stack Development Certificate",
-            description: "Provided by InternPe",
-            icon: "google",
-            link: "https://drive.google.com/file/d/10p5bLZeaGV3FhI8sFLA7aVxFywo8fKES/view?usp=drive_link",
-        },
-        {
-            title: "Web Development",
-            description: "Provided by Techienest",
-            icon: "techienest_logo.jpeg",
-            link: "https://drive.google.com/file/d/1AiJ09mlHyxIcJj6nPw-Gnv9oMA8rto2i/view?usp=drive_link"
-        },
-       
     //     {
     //         title: "Software Engineer Certificate",
     //         description: "Provided by HackerRank",
     //         icon: "hackerrank",
     //         link: "https://www.hackerrank.com/certificates/efdbdd44eb7d"
     //     }
-     ],
+  ],
 
-    // ============ CONTACT ============
-    contact: {
-        title: "Let's Get in Touch: Ways to Connect with Me",
-        description: "Thank you for your interest in getting in touch with me. I welcome your feedback, questions, and suggestions.As a full-stack developer, I’m always open to discussing exciting projects, collaborations, or any tech-related queries. If you’d like to connect, feel free to reach out through my social media links below. I look forward to engaging with like-minded professionals and tech enthusiasts!",
-    },
+  // ============ CONTACT ============
+  contact: {
+    title: "Let's Get in Touch: Ways to Connect with Me",
+    description:
+      "Thank you for your interest in getting in touch with me. I welcome your feedback, questions, and suggestions.As a full-stack developer, I’m always open to discussing exciting projects, collaborations, or any tech-related queries. If you’d like to connect, feel free to reach out through my social media links below. I look forward to engaging with like-minded professionals and tech enthusiasts!",
+  },
 
-    // ============ FOOTER ============
-    footer: "© 2025 Avinash Jat. All Rights Reserved"
-}
+  // ============ FOOTER ============
+  footer: "© 2025 Avinash Jat. All Rights Reserved",
+};
 
-export default info
+export default info;
